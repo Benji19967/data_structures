812e4733d5d7afa3e052b64c16c54d0e81ddf64d
Benjamin Labrecque <35387157+Benji19967@users.noreply.github.com>
2019-03-28 09:34:45 -0400

    Update singly_linked_list.cpp

--- singly_linked_list.cpp
+++ singly_linked_list.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 class Node {
 	public:
-	    int data;
-	    Node* next;
+		int data;
+		Node* next;
 
-        Node(int data) {
-            this->data = data;
-            this->next = nullptr;
-        }
+		Node(int data) {
+			this->data = data;
+			this->next = nullptr;
+		}
 };
 
 class SinglyLinkedList {
 	public:
-        Node* head;
-        Node* tail;
+		Node* head;
+		Node* tail;
 
-        SinglyLinkedList() {
-            this->head = nullptr;
-            this->tail = nullptr;
-        }
+		SinglyLinkedList() {
+			this->head = nullptr;
+			this->tail = nullptr;
+		}
 };
 
 int main() {
