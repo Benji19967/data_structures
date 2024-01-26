19f85e7d40c16ec60e363e1062eee34babf58b6a
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-01-26 15:28:28 +0100

    Add test for dequeue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -8,3 +8,16 @@ def test_enqueue():
     q.enqueue(5)
     assert len(q) == 1
     assert q._queue[0] == 5
+
+
+def test_dequeue():
+    q = Queue()
+    assert len(q) == 0
+
+    q.enqueue(7)
+    assert len(q) == 1
+    assert q._queue[0] == 7
+
+    element = q.dequeue()
+    assert element == 7
+    assert len(q) == 0
