e4d0dd61897ca2f7b7dbfd2a93e0f58eb9aee058
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-05-08 20:11:20 +0200

    Reformat to use the standard 2 spaces suggested by Google formatting style

--- ds_cpp/singly_linked_list.cpp
+++ ds_cpp/singly_linked_list.cpp
@@ -3,43 +3,43 @@
 using namespace std;
 
 class Node {
-    public:
-        int data;
-        Node* next;
+public:
+  int data;
+  Node *next;
 
-        Node(int data) {
-            this->data = data;
-            this->next = nullptr;
-        }
+  Node(int data) {
+    this->data = data;
+    this->next = nullptr;
+  }
 };
 
 class SinglyLinkedList {
-    public:
-        Node* head;
-        Node* tail;
+public:
+  Node *head;
+  Node *tail;
 
-        SinglyLinkedList() {
-            this->head = nullptr;
-            this->tail = nullptr;
-        }
+  SinglyLinkedList() {
+    this->head = nullptr;
+    this->tail = nullptr;
+  }
 };
 
 int main() {
-    SinglyLinkedList* l = new SinglyLinkedList();
+  SinglyLinkedList *l = new SinglyLinkedList();
 
-    Node* n1 = new Node(1);
-    Node* n2 = new Node(2);
-    Node* n3 = new Node(3);
+  Node *n1 = new Node(1);
+  Node *n2 = new Node(2);
+  Node *n3 = new Node(3);
 
-    n1->next = n2;
-    n2->next = n3;
+  n1->next = n2;
+  n2->next = n3;
 
-    l->head = n1;
+  l->head = n1;
 
-    cout << l->head->next->next->data; // prints 3
-    
-    delete n1;
-    delete n2;
-    delete n3;
-    delete l;
+  cout << l->head->next->next->data; // prints 3
+
+  delete n1;
+  delete n2;
+  delete n3;
+  delete l;
 }
