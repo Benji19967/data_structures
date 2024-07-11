9e1fe04fc817a1736dd97bff93c74cd521090916
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-07-11 11:04:47 +0200

    Add C arrays file

---
+++ ds_c/array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define N 10
+
+void print_array(int arr[], int arr_length) {
+  for (int i = 0; i < arr_length; i++) {
+    printf("%d: %d\n", i, arr[i]);
+  }
+}
+
+int main() {
+  int arr[N] = {0};
+  int arr_length = sizeof(arr) / sizeof(arr[0]);
+
+  print_array(arr, arr_length);
+}
