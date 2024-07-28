62b43314ffaca69e95eb7f8e44349e8774706516
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-07-28 19:39:47 +0200

    Add example unit tests using unity

---
+++ ds_c/test/test_array.c
@@ -0,0 +1,30 @@
+#include "../unity/src/unity.h"
+
+void setUp(void) {
+  // set stuff up here
+}
+
+void tearDown(void) {
+  // clean stuff up here
+}
+
+void test_add() {
+  int output = 10 + 20;
+  TEST_ASSERT_EQUAL(30, output);
+}
+
+void test_subtract() {
+  int output = 10 - 20;
+  TEST_ASSERT_EQUAL(-9, output);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_add);
+  RUN_TEST(test_subtract);
+
+  UNITY_END();
+
+  return 0;
+}
