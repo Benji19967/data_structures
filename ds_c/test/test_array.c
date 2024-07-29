58edc4e9272a1ca2bc03b2b452003c2242110a09
Benjamin Labrecque <labrecque.benji@gmail.com>
2024-07-29 09:30:22 +0200

    Add more meaningful array test

--- ds_c/test/test_array.c
+++ ds_c/test/test_array.c
@@ -8,21 +8,17 @@ void tearDown(void) {
   // clean stuff up here
 }
 
-void test_add() {
-  int output = 10 + 20;
-  TEST_ASSERT_EQUAL(30, output);
-}
-
-void test_subtract() {
-  int output = 10 - 20;
-  TEST_ASSERT_EQUAL(-8, output);
+void test_create_array_of_pointers() {
+  int *arr[3] = {NULL};
+  int *arr2[3] = {NULL, NULL, NULL};
+  TEST_ASSERT_EQUAL_INT_ARRAY(arr, arr2, 3);
+  TEST_ASSERT_EACH_EQUAL_PTR_MESSAGE(NULL, arr, 3, "Not all pointers are NULL");
 }
 
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(test_add);
-  RUN_TEST(test_subtract);
+  RUN_TEST(test_create_array_of_pointers);
 
   UNITY_END();
 
